e13299faf52834519fd04d20732f23da66216683
Nouman <noumanfarooq387@gmail.com>
2025-06-13 22:53:01 +0500

    creating server through express

--- CRUD/index.js
+++ CRUD/index.js
@@ -1,1 +1,17 @@
-console.log("Hi")
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+
+const PORT = 7000;
+
+app.get('/user',(req, res)=>{
+    res.send("Hi")
+})
+
+app.get('/login',(req, res)=>{
+    res.send("Login Hi!!!!")
+})
+app.listen(PORT, ()=>{
+    console.log(`App run at Port ${PORT}`);
+})
